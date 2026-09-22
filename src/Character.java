3c994e75bb9146ef3a244fd53ce6b3524210e435
kunaltomar10 <kunaltomar2910@gmail.com>
2026-09-22 13:41:33 +0530

    Added java string program

---
+++ src/Character.java
@@ -0,0 +1,13 @@
+
+public class Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String txt= "Asterics Computer Institute";
+		
+		System.out.println("Character at 0 position is "+txt.charAt(0));
+		System.out.println("Character at 10 position is "+txt.charAt(10));
+
+	}
+
+}
---
+++ src/UnicodePoint.java
@@ -0,0 +1,14 @@
+
+public class UnicodePoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String school="asterisc.in";
+		
+		int count=school.codePointCount(0,school.length());
+		System.out.println("Code point count ="+count);
+		
+
+	}
+
+}
